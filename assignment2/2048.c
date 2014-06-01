03839cb3095c6b72e5ad798828ce243d7835c4e2
Danilo Scodellaro <dan.scodellaro@gmail.com>
2014-06-02 00:32:08 +1000

    All Dryrun Tests Passed
    LESS THAN 6 HOURS!

--- assignment2/2048.c
+++ assignment2/2048.c
@@ -1,17 +1,46 @@
+// Author: Danilo Scodellaro
+// z3414551
+// Date created: 01-JUN-2014
+// Date submitted: 02-JUN-2014
+// cs1911
+// Assignment 2 - 2048
+// 2048.C
 
-// add your header comment here
+// DESCRIPTION
+// Program implements the recently developed game 2048
+// Javascript web application available at:
+// "http://gabrielecirulli.github.io/2048/"
+
+// CONSTRAINTS
+// The incomplete file 2048.c is provided as a starting point
+// Some parts of the program cannot be edited
+// Specifically: main, insertNewNumber, printHelp
+// 8 functions MUST be implemented (type cannot change)
+// Specifically: moveXXXX, boardContains2048, printBoard, readBoard
+// insertNewNumber depends on vacant squares containing the value 0
+// Code must work if the SIZE constant is changes (see #define)
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 #include <time.h>
-// put any extra includes here, but don't delete the ones above
 
-#define SIZE    4
+#define SIZE 4
+#define GOAL 2048
 
-// add your function_prototypes here 
+// Function Prototypes
 
+int moveLeft(int board[SIZE][SIZE]);
+int moveRight(int board[SIZE][SIZE]);
+int moveDown(int board[SIZE][SIZE]);
+int moveUp(int board[SIZE][SIZE]);
+int gameOver(int board[SIZE][SIZE]);
+int boardContains2048(int board[SIZE][SIZE]);
+void printBoard(int board[SIZE][SIZE]);
+int readBoard(int board[SIZE][SIZE]);
+void printHelp(void);                           //DO NOT MODIFY
+void insertNewNumber(int board[SIZE][SIZE]);    //DO NOT MODIFY
 
 // The functions moveLeft, moveRight, moveUp, moveDown
 // return -1 if the specified moving numbers  is not possible.
@@ -20,61 +49,398 @@
 // They return 0 if no numbers were combined.
 
 int moveLeft(int board[SIZE][SIZE]) {
-    return -1;
+
+    //Initialise Variables
+    int i = 0;
+    int j = 0;
+    int moveScore = 0;
+    int changeFlag = 1;
+    int changeCounter = 0;
+
+    //Move Left (Shift to edge)
+    changeFlag = 1;
+    while (changeFlag == 1) {
+        changeFlag = 0;
+        for (i = 0; i < SIZE; i++) {
+            for (j = SIZE - 1; j >= 0; j--) {
+                if (j == 0 || board[i][j] == 0) {
+                    continue;
+                } else if (board[i][j-1] == 0) {
+                    board[i][j-1] = board[i][j];
+                    board[i][j] = 0;
+                    changeFlag = 1;
+                    changeCounter = changeCounter + 1;
+                }
+            }
+        }
+    }
+
+    //Move Left (Additive)
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            if (j == 0 || board[i][j] == 0) {
+                continue;
+            } else if (board[i][j] == board[i][j-1]) {
+                board[i][j-1] = 2*board[i][j-1];
+                board[i][j] = 0;
+                moveScore = moveScore + board[i][j-1];
+            }
+        }
+    }
+
+    //Move Left (Shift to edge)
+    changeFlag = 1;
+    while (changeFlag == 1) {
+        changeFlag = 0;
+        for (i = 0; i < SIZE; i++) {
+            for (j = SIZE - 1; j >= 0; j--) {
+                if (j == 0 || board[i][j] == 0) {
+                    continue;
+                } else if (board[i][j-1] == 0) {
+                    board[i][j-1] = board[i][j];
+                    board[i][j] = 0;
+                    changeFlag = 1;
+                    changeCounter = changeCounter + 1;
+                }
+            }
+        }
+    }
+
+    //Return score to main
+    if (moveScore == 0 && changeCounter == 0) {
+        return -1; 
+    } else {
+        return moveScore;
+    } 
 }
 
 int moveRight(int board[SIZE][SIZE]) {
-    return -1;
+
+    //Initialise Variables
+    int i = 0;
+    int j = 0;
+    int moveScore = 0;
+    int changeFlag = 1;
+    int changeCounter = 0;
+
+    //Move Right (Shift to edge)
+    changeFlag = 1;
+    while (changeFlag == 1) {
+        changeFlag = 0;
+        for (i = 0; i < SIZE; i++) {
+            for (j = 0; j < SIZE; j++) {
+                if (j == SIZE - 1 || board[i][j] == 0) {
+                    continue;
+                } else if (board[i][j+1] == 0) {
+                    board[i][j+1] = board[i][j];
+                    board[i][j] = 0;
+                    changeFlag = 1;
+                    changeCounter = changeCounter + 1;
+                }
+            }
+        }
+    }
+
+    //Move Right (Additive)
+    for (i = 0; i < SIZE; i++) {
+        for (j = SIZE - 1; j >= 0; j--) {
+            if (j == SIZE - 1 || board[i][j] == 0) {
+                continue;
+            } else if (board[i][j] == board[i][j+1]) {
+                board[i][j+1] = 2*board[i][j+1];
+                board[i][j] = 0;
+                moveScore = moveScore + board[i][j+1];
+            }
+        }
+    }
+
+    //Move Right (Shift to edge)
+    changeFlag = 1;
+    while (changeFlag == 1) {
+        changeFlag = 0;
+        for (i = 0; i < SIZE; i++) {
+            for (j = 0; j < SIZE; j++) {
+                if (j == SIZE - 1 || board[i][j] == 0) {
+                    continue;
+                } else if (board[i][j+1] == 0) {
+                    board[i][j+1] = board[i][j];
+                    board[i][j] = 0;
+                    changeFlag = 1;
+                    changeCounter = changeCounter + 1;
+                }
+            }
+        }
+    }
+
+    //Return score to main
+    if (moveScore == 0 && changeCounter == 0) {
+        return -1; 
+    } else {
+        return moveScore;
+    } 
 }
 
 int moveDown(int board[SIZE][SIZE]) {
-    return -1;
+
+    //Initialise Variables
+    int i = 0;
+    int j = 0;
+    int moveScore = 0;
+    int changeFlag = 1;
+    int changeCounter = 0;
+
+    //Move Down (Shift to edge)
+    changeFlag = 1;
+    while (changeFlag == 1) {
+        changeFlag = 0;
+        for (j = 0; j < SIZE; j++) {
+            for (i = 0; i < SIZE; i++) {
+                if (i == SIZE - 1 || board[i][j] == 0) {
+                    continue;
+                } else if (board[i+1][j] == 0) {
+                    board[i+1][j] = board[i][j];
+                    board[i][j] = 0;
+                    changeFlag = 1;
+                    changeCounter = changeCounter + 1;
+                }
+            }
+        }
+    }
+
+    //Move Down (Additive)
+    for (j = 0; j < SIZE; j++) {
+        for (i = SIZE - 1; i >= 0; i--) {
+            if (i == SIZE - 1 || board[i][j] == 0) {
+                continue;
+            } else if (board[i][j] == board[i+1][j]) {
+                board[i+1][j] = 2*board[i+1][j];
+                board[i][j] = 0;
+                moveScore = moveScore + board[i+1][j];
+            }
+        }
+    }
+
+    //Move Down (Shift to edge)
+    changeFlag = 1;
+    while (changeFlag == 1) {
+        changeFlag = 0;
+        for (j = 0; j < SIZE; j++) {
+            for (i = 0; i < SIZE; i++) {
+                if (i == SIZE - 1 || board[i][j] == 0) {
+                    continue;
+                } else if (board[i+1][j] == 0) {
+                    board[i+1][j] = board[i][j];
+                    board[i][j] = 0;
+                    changeFlag = 1;
+                    changeCounter = changeCounter + 1;
+                }
+            }
+        }
+    }
+
+    //Return score to main
+    if (moveScore == 0 && changeCounter == 0) {
+        return -1; 
+    } else {
+        return moveScore;
+    } 
 }
 
 int moveUp(int board[SIZE][SIZE]) {
-    return -1;
+
+    //Initialise Variables
+    int i = 0;
+    int j = 0;
+    int moveScore = 0;
+    int changeFlag = 1;
+    int changeCounter = 0;
+
+    //Move Up (Shift to edge)
+    changeFlag = 1;
+    while (changeFlag == 1) {
+        changeFlag = 0;
+        for (j = 0; j < SIZE; j++) {
+            for (i = SIZE - 1; i >= 0; i--) {
+                if (i == 0 || board[i][j] == 0) {
+                    continue;
+                } else if (board[i-1][j] == 0) {
+                    board[i-1][j] = board[i][j];
+                    board[i][j] = 0;
+                    changeFlag = 1;
+                    changeCounter = changeCounter + 1;
+                }
+            }
+        }
+    }
+
+    //Move Up (Additive)
+    for (j = 0; j < SIZE; j++) {
+        for (i = 0; i < SIZE; i++) {
+            if (i == 0 || board[i][j] == 0) {
+                continue;
+            } else if (board[i][j] == board[i-1][j]) {
+                board[i-1][j] = 2*board[i-1][j];
+                board[i][j] = 0;
+                moveScore = moveScore + board[i-1][j];
+            }
+        }
+    }
+
+    //Move Up (Shift to edge)
+    changeFlag = 1;
+    while (changeFlag == 1) {
+        changeFlag = 0;
+        for (j = 0; j < SIZE; j++) {
+            for (i = SIZE - 1; i >= 0; i--) {
+                if (i == 0 || board[i][j] == 0) {
+                    continue;
+                } else if (board[i-1][j] == 0) {
+                    board[i-1][j] = board[i][j];
+                    board[i][j] = 0;
+                    changeFlag = 1;
+                    changeCounter = changeCounter + 1;
+                }
+            }
+        }
+    }
+
+    //Return score to main
+    if (moveScore == 0 && changeCounter == 0) {
+        return -1; 
+    } else {
+        return moveScore;
+    } 
 }
 
 // gameOver returns 0 iff it is possible to make a move on the board
 // It returns 1 otherwise.
 
 int gameOver(int board[SIZE][SIZE]) {
-    return 0;
+
+    //Initialise Variables
+    int i = 0;
+    int j = 0;
+    int boardClone[SIZE][SIZE] = {{0}};
+    int moveScore = 0;
+
+    //Copy board to boardClone
+    for (j = 0; j < SIZE; j++) {
+        for (i = 0; i < SIZE; i++) {
+            boardClone[i][j] = board[i][j];
+        }
+    }
+
+    //Attempt all moves on boardClone
+    moveScore = moveScore + moveLeft(boardClone);
+    moveScore = moveScore + moveDown(boardClone);
+    moveScore = moveScore + moveUp(boardClone);
+    moveScore = moveScore + moveRight(boardClone);
+
+    //Return score to main
+    if (moveScore == -4) {
+        return 1; 
+    } else {
+        return 0;
+    } 
+
 }
 
 // boardContains2048 returns 1 iff the board contains 2048.
 // It returns 0 otherwise.
 
 int boardContains2048(int board[SIZE][SIZE]) {
+
+    //Initialise Variables
+    int i = 0;
+    int j = 0;
+
+    //Check if board contains GOAL(2048)
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {   
+            if ((board[i][j]) == GOAL) {
+                return 1;
+            }
+        }
+    } 
+
     return 0;
 }
 
 // printBoard displays the board.
 
 void printBoard(int board[SIZE][SIZE]) {
-}
 
+    //Initialise Variables
+    int i = 0;
+    int j = 0;
+    char zero = '.';
+    int k = 0;          //Border Counter
+    int m = 0;          //Empty Space Counter
+
+    //Print Top Border
+    printf("+");
+    for (k = 0; k < SIZE; k++) {
+        printf("------");
+    }
+    printf("+\n");
+
+    //Print Board
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            if (j == 0) {
+                printf("|");
+            }
+            if (board[i][j] == 0) {
+                printf("%5c ", zero);
+            } else {
+                printf("%5d ", board[i][j]);
+            }
+            if (j == SIZE - 1) {
+                printf("|\n");
+            }
+        }
+
+        //Print Empty Line
+        if (i != SIZE - 1) {
+            printf("|     ");
+            for (m = 1; m < SIZE; m++){
+                printf("      ");
+            }
+            printf(" |\n");
+        }
+    }
+
+    //Print Bottom Border
+    printf("+");
+    for (k = 0; k < SIZE; k++) {
+        printf("------");
+    }
+    printf("+\n");
+}
 
 // readBoard attempts to read SIZE*SIZE integers into array board
 // it returns how many integers actually were read
 
 int readBoard(int board[SIZE][SIZE]) {
-    return 0;
+
+    //Initialise Variables
+    int i = 0;
+    int j = 0;
+    int numbersRead = 0;
+
+    //Load user input into board array
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            scanf("%d", &board[i][j]);
+            numbersRead = numbersRead + 1;            
+        }
+    }    
+
+    return numbersRead;
 }
 
-//
-// add your functions here
-//
-
-//
-// You do not need to modify the code below here.
-//
-// If you wish to modify the code below, you have
-// misunderstood the assignment specification
-//
-
-void printHelp(void);
-void insertNewNumber(int board[SIZE][SIZE]);
+// DO NOT MODIFY CODE BELOW THIS LINE 
+// Code not modified to remain under 80 column limit - currently exceeds limit
 
 int main(int argc, char *argv[]) {
     int board[SIZE][SIZE] = {{0}};
